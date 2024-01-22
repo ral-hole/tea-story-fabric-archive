da876acd73e0f60f091793ec46f54fd450e03d73
江湖之洞 <ral-hole@outlook.com>
2024-01-22 21:45:53 +0800

    Initial commit

---
+++ src/main/java/cloud/lemonslice/teastory/TeaStoryClient.java
@@ -0,0 +1,10 @@
+package cloud.lemonslice.teastory;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TeaStoryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/cloud/lemonslice/teastory/TeaStoryDataGenerator.java
@@ -0,0 +1,11 @@
+package cloud.lemonslice.teastory;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TeaStoryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
